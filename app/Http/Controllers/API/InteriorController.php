0a585511c9e5a1c4224778879cc85a4bc6007bd9
Kenlie422023014 <151435095+Kenlie422023014@users.noreply.github.com>
2024-06-07 18:07:04 +0700

    axios

--- app/Http/Controllers/API/InteriorController.php
+++ app/Http/Controllers/API/InteriorController.php
@@ -26,13 +26,111 @@ use App\Models\Interior;
       *         response=200,
       *         description="successful",
       *         @OA\JsonContent()
-      *     )
+      *     ),
+      *     @OA\Parameter(
+      *     name="_page",
+      *     in="query",
+      *     description="current page",
+      *     required= true,
+      *         @OA\Schema(
+      *             type="integer",
+      *             format="int64",
+      *             example=1
+      *         )
+      *     ),
+      *     @OA\Parameter(
+      *     name="_limit",
+      *     in="query",
+      *     description="max item in a page",
+      *     required= true,
+      *         @OA\Schema(
+      *             type="integer",
+      *             format="int64",
+      *             example=10
+      *         )
+      *     ),
+      *     @OA\Parameter(
+      *     name="_search",
+      *     in="query",
+      *     description="word to search",
+      *     required= false,
+      *         @OA\Schema(
+      *             type="string",
+      *         )
+      *     ),
+      *     @OA\Parameter(
+      *     name="_producer",
+      *     in="query",
+      *     description="search by producer name",
+      *     required= false,
+      *         @OA\Schema(
+      *             type="string",
+      *         )
+      *     ),
+      *     @OA\Parameter(
+      *     name="_sort_by",
+      *     in="query",
+      *     description="word to search",
+      *     required= false,
+      *         @OA\Schema(
+      *             type="integer",
+      *             example="latest"
+      *         )
+      *     ),
       * )
       */
-     public function index()
+     public function index(Request $request)
      {
-         return Interior::get();
-     }
+       try {
+            $data['filter'] = $request ->all();
+            $page           = $data['filter']['_page'] =(@$data['filter']['_page'] ? intval($data['filter']['_page']) : 1);
+            $limit          = $data['filter']['_limit'] =(@$data['filter']['_limit'] ? intval($data['filter']['_limit']) : 1000);
+            $offset         = ($page?($page-1)*$limit:0);
+            $data['products']= Interior::whereRaw('1 = 1');
+
+            if($request->get('_search')){
+                $data['products'] = $data['products']->whereRaw('(LOWER(title) LIKE "%'.strtolower($request->get('_search')).'%" 
+                OR LOWER (Producer) LIKE "%'.strtolower($request->get('_search')).'%")');
+            }
+
+            if ($request->get('_producer')){
+                $data ['products']=$data ['products']->whereRaw ('LOWER(producer)="'.strtolower($request->get('_producer')).'"');
+            }
+            
+            if ($request->get('_sort_by')){
+                switch ($request->get('_sort_by')) {
+                    default:
+                    case 'latest_Producer':
+                    $data['product']= $data['products']->orderBy('Year_produced');
+                    break;
+                    case 'latest_added':
+                    $data['products'] = $data['products']->orderBy('created_at');
+                    break;
+                    case 'title_asc':
+                    $data['products'] = $data['products']->orderBy('name','ASC');
+                    break;
+                    case 'title_desc':
+                    $data['products'] = $data['products']->orderBy('name','DESC');
+                    break;
+                    case 'price_asc':
+                    $data['products'] = $data['products']->orderBy('price','ASC');
+                    break;
+                    case 'price_desc':
+                    $data['products'] = $data['products']->orderBy('price','DESC');
+                    break;
+                }}
+                 $data['products_count_total'] = $data['products']->count();
+                 $data['products']             = ($limit==0 && $offset==0)?$data['product']:$data['products']->limit($limit)->offset($offset);
+                 // $data['products_raw_sql']  = $data['products]->toSql();
+                 $data['products']             = $data['products']->get();
+                 $data['products_count_start'] = ($data['products_count_total'] == 0 ? 0 : (($page-1)*$limit)+1);
+                 $data['products_count_end']   = ($data['products_count_total'] == 0 ? 0 : (($page-1)*$limit)+sizeof($data['products']));
+                 return response()->json($data,200);
+
+        }       catch (\Exception $exception) {
+            throw new HttpException(400, "invalid data : {$exception->getMessage()}");
+        }
+    }
  
      /**
       * @OA\Post(
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app-public')
+@section('title','home')
+@section('content')
+    <div class="site-wrapper-reveal">
+        <div class="hero-box-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- Hero Slider Area Start -->
+                        <div class="hero-area" id="product-preview">
+                        </div>
+                        <!-- Hero Slider Area End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div class="about-us-area section-space--ptb_120">
+            <div class="container">
+                <div class="row">
+                    <div class="about-us-content_6 text-center">
+                        <h2>Pandora Interior.&nbsp;&nbsp;Store</h2>
+                        <p>
+                            <small>
+                            Welcome to Pandora Interior, your premier destination for all your elegant and functional home interior needs. 
+                            We offer a wide range of high-quality products, from modern furniture to unique decor items that will transform every room into your dream space. With a collection that always follows the latest trends and is designed by experts, we ensure that every item you choose not only enhances the aesthetics but also adds comfort to your home. Explore our extensive catalog and find inspiration to create a captivating and charming atmosphere in every corner of your home. 
+                            Shop easily, securely, and satisfyingly only at Pandora Interior.
+                            </small>
+                        </p>
+                        <p class="mt-5">Find more about us!
+                            <span class="text-color-primary"> Our other website.</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Banner Video Area Start -->
+        <div class="banner-video-area overflow hidden">
+            <div class="container">
+                <div class="row">
+                    <div class="banner-video-box">
+                        <img src="https://www.magd.ox.ac.uk/wp-content/uploads/2022/07/3027-052-2000x1000.jpg" alt="">
+                        <div class="video-icon">
+                            <a href="https://www.youtu.be/Na5KPnx0uS8?" class="popup-youtube">
+                            <i class="linear-ic-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+        <!-- Banner Video Area end -->
+
+        <!--our member area start -->
+        <div class="our member-area section-space--pb_120">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="member--box">
+                            <div class="row align-items-center">
+                                <div class= "col-lg-5 col-md-4">
+                                    <div class="section-title small-mb__40 
+                                    tablet -mb__40">
+                                        <h4 class="section title"> Join our membership </h4>
+                                        <p>Get Special Discount</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-7 col-md-8">
+                                    <div class="member-wrap">
+                                        <form action="#" class= "member--two">
+                                            <input class="input-box" type="text"
+                                            placeholder="Your email address">
+                                            <button class="submit-btn"><i 
+                                            class="icon-arrow-right"></i></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--our member area End -->
+</div>
+@endsection
+@section('addition_css')
+@endsection
+@section('addition_script')
+    <script src="{{asset('pages\js/home.js')}}"></script>
+@endsection
